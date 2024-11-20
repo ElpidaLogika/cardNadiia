7527307164d0ec4c4a7f57b44b8452505a8bfbcd
elpidalogika <elpidalogika@gmail.com>
2024-11-20 18:20:25 +0200

    change name, create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let git = document.querySelector('.github');
+let inst = document.querySelector('.inst');
+
+
+git.addEventListener('click', function () {
+    location.assign('https://github.com/ElpidaLogika')
+});
+
+inst.addEventListener('click', function () {
+    location.assign('https://www.instagram.com/')
+});
+
